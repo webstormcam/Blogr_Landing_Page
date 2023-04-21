1a05654a86b6e2dd0b9457642a10b7a3844ab41a
Cameron Chandler <webstormcam@gmail.com>
2023-04-20 20:49:10 -0400

    Removed the not important code

--- script.js
+++ script.js
@@ -1,46 +1,5 @@
 let ham = document.getElementById('ham');
 let mobileMenu = document.getElementById('mobile-menu');
-let miniDrop = document.querySelectorAll('.mini-option-picker');
-let miniLinks = document.querySelectorAll('.option-menu')
-let miniMenus = document.querySelectorAll('.small-drop-down');
-
-
-for(let i=0;i<miniDrop.length;i++){
-    miniDrop[i].addEventListener('click', handleClick);
-        function handleClick(e){
-            let el = e.currentTarget;
-          if( el.children[1].classList.contains('drop-picked')){
-                el.children[1].classList.remove('drop-picked');
-                miniMenus[i].children[1].classList.remove('appear');
-                
-            } else{
-                el.children[1].classList.add('drop-picked')
-                miniMenus[i].children[1].classList.add('appear');
-               
-            }
-            for(let q=0;q<miniDrop.length;q++){
-                if(miniDrop[q]!==el){
-                  miniDrop[q].children[1].classList.remove('drop-picked');
-                  miniMenus[q].children[1].classList.remove('appear')
-                }
-            }
-
-
-    
-        }
-        
-     
-     
-
-    
-}
-
-
-
-
-
-
-
 ham.addEventListener('click',clickedHam);
 function clickedHam(){
     if(ham.getAttribute('src')==='imgs/icon-hamburger.svg'){
@@ -55,4 +14,3 @@ function clickedHam(){
 }
 
 
-// LOL
